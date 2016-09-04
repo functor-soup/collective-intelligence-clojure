3cd6309febb52b631962011dddf0e8057dac0dcf
luke <nerorevenge@gmail.com>
2016-09-04 15:18:07 -0400

    added combinatorics library

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
+                 [org.clojure/math.combinatorics "0.1.3"]
                  [http-kit "2.1.18"]])
