c94dcb4094d7e4d3f832587a7cedee969e6e6dd6
luke <nerorevenge@gmail.com>
2016-09-05 23:36:46 -0400

    Recommendations function is working

--- src/mach_lear/chapter-two.clj
+++ src/mach_lear/chapter-two.clj
@@ -1,11 +1,13 @@
 (ns mach-lear.chapter-two
   (:require [clojure.set :as set]
-            [mach-lear.metric :as metric]))
+            [mach-lear.metric :as metric]
+            [mach-lear.utility :as utility]))
 
 ;; forming the data, doing it functionally
 ;; because repetition is boring
 (defonce movies ["Lady in the Water","Snakes on a Plane", "Just My Luck",
                  "Superman Returns", "You, Me and Dupree", "The Night Listener"])
+
 
 (defonce critics {"Lisa Rose"        [2.5 3.5 3.0 3.5 2.5 3.0]
                   "Gene Seymour"     [3.0 3.5 1.5 5.0 3.5 3.0]
@@ -16,6 +18,7 @@
                   "Toby"             [nil 4.5 nil 4.0 1.0 nil]
                   })
 
+
 ;; todo: refactor this
 (def data (letfn ([clean [x]
                    (->> x
@@ -25,47 +28,59 @@
                 (map #(-> [(first %) (clean (zipmap movies (second %)))]))
                 (into {}))))
 
+
 (defn sim-distance
   [& {:keys [metric] :or {metric metric/pearson-score}}]
-  (fn [data person1 person2]
-    (let [person1_ (data person1)
-        person2_ (data person2)
-        common (map-keys-intersection person1_ person2_)
-        person1_filtered (vals  (select-keys person1_ common))
-        person2_filtered (vals  (select-keys person2_ common))
-        input (map vector person1_filtered person2_filtered)]
-       (apply metric input)
-    )))
+  (fn [person1-map person2-map]
+    (let [common (utility/get-common-keys person1-map person2-map)
+          person1_filtered (vals  (select-keys person1-map common))
+          person2_filtered (vals  (select-keys person2-map common))]
+          (metric person1_filtered person2_filtered)
+       )))
+
 
 (def similarity-pearson   (sim-distance))
-(def similarity-euclidean (sim-distance metric/modified-euclidean))
-(def similarity-manhattan (sim-distance metric/manhattan-distance))
-(def similarity-jaccard   (sim-distance metric/jaccard-similarity))
+(def similarity-euclidean (sim-distance :metric metric/modified-euclidean))
+(def similarity-manhattan (sim-distance :metric metric/manhattan-distance))
+(def similarity-jaccard   (sim-distance :metric metric/jaccard-similarity))
 
 
-(defn topMatches
+(defn- get-similarity-map
+  "giet map of similarities to a person based on a metric"
+  [data person & {:keys [similarity] :or {similarity similarity-pearson}}]
+  (let [rest (set/difference (set (keys data)) #{person})
+        person-map (data person)]
+      (->> rest
+           (map #(->[%,(similarity (data %) person-map)]))
+           (into {}))))
+
+
+(defn top-matches
   [prefs person & {:keys [n similarity] :or {n 5 similarity similarity-pearson}}]
-  (let [ rest (set/difference (set (keys prefs)) #{person}) ]
-    (->> rest
-         (map #(->[%,(similarity prefs % person)]))
-         (into {})
+  (->> (get-similarity-map prefs person :metric similarity)
          (sort-by val >)
-         (take n)
-    )))
-
-(defn getRecommendations
-  [data person & {:keys [similarity] :or {similarity sim-distance}}]
-  (let [others (set/difference (set (keys data)) #{person})
-        others-similarity (into {} (map #(-> [%,(sim-distance data person %)]) others)) ; map of person -> similarity 
-        items (keys (data person))
-        totals (make-hash-map items 0)
-        simSum (make-hash-map items 0)]
-    (reduce-kv (fn [m k v] (do  (map-operation-on-vals + (:totals m) (k data))))
-                     {:totals totals :simSum simSum} others-similarity )))
+         (take n)))
 
 
-;;(println (sim-distance data "Lisa Rose" "Gene Seymour"))
-(getRecommendations data "Toby")
+(defn get-recommendations
+  [data list-of-items person & {:keys [similarity] :or {similarity similarity-pearson}}]
+  (let [others-similarity  (get-similarity-map data person :similarity similarity)
+        items   (set/difference (set list-of-items) (set (keys (data person))))
+        totals  (utility/make-hash-map items 0)
+        simSum  (utility/make-hash-map items 0)]
+    (->> (reduce-kv
+          (fn [m k v]
+            (-> m
+              (assoc :totals (utility/map-union #(+ %1 (* v %2)) (m :totals)  (data k)))
+              (assoc :simSum (utility/map-union + (m :simSum) (utility/make-hash-map (keys (data k)) v)))))
+          {:totals totals :simSum simSum} others-similarity)
+        (vals)
+        (map #(select-keys % items))
+        (apply utility/map-intersection / )
+        (sort-by val >))))
+
+
+(println (get-recommendations data movies "Toby"))
 
 
 
