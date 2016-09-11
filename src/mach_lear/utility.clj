9aa96d75d2077d16f656719e9c6edd1803722909
luke <nerorevenge@gmail.com>
2016-09-11 00:28:45 -0400

    CHnages in utility file  - added more tests

--- src/mach_lear/utility.clj
+++ src/mach_lear/utility.clj
@@ -17,36 +17,47 @@
 
 (defn map-difference
   "map version for set difference"
-  [& args]
-  (let [main-map (nth args 0)
-        difference (apply set/difference (map #(set (keys %)) args))]
-    (reduce (fn [a x]
-              (assoc a x (main-map x)))
-            {} difference)))
+  ([] {})
+  ([& args]
+     (let [main-map   (nth args 0)
+           difference (apply set/difference (map #(set (keys %)) args))]
+      (reduce (fn [a x]
+                (assoc a x (main-map x)))
+              {} difference))))
 
 (defn map-intersection
   "f over values for map intersection"
-  [f & args]
-  (let [common (apply get-common-keys args)]
-    (reduce (fn [a x]
-              (assoc a x (apply f (map #(% x) args))))
-            {} common)))
+  ([] {})
+  ([arg] arg)
+  ([f & args]
+    (let [common (apply get-common-keys args)]
+      (reduce (fn [a x]
+                (assoc a x (apply f (map #(% x) args))))
+              {} common))))
 
 (defn map-union
   "combines all maps into one
   big fat map"
-  [f & args]
-  (let [keys (->> args
-                  (map keys) (flatten) (distinct))]
-    (reduce (fn [a x]
-              (let [k (remove nil? (map #(% x) args))]
-                (if (> (count k) 1)
-                  (assoc a x (apply f k))
-                  (assoc a x (nth k 0))))) {} keys)))
+  ([] {})
+  ([arg] arg)
+  ([f & args]
+    (let [keys (->> args
+                  (map keys) (flatten)
+                  (remove nil?) (distinct))]
+      (reduce (fn [a x]
+                (let [k (remove nil? (map #(% x) args))]
+                  (if (> (count k) 1)
+                    (assoc a x (apply f k))
+                    (assoc a x (nth k 0))))) {} keys))))
 
 (defn map-xor
   "gives back a vector of maps with non-common
   key-value pairs"
-  [& args]
-  (let [permutations (combo/permutations args)]
-    (distinct  (reduce (fn [a x] (conj a (apply map-difference x))) [] permutations))))
+  ([] {})
+  ([& args]
+    (if (= (count (distinct args)) 1)
+      (take (count args) (repeat {}))
+      (let [permutations (combo/permutations args)]
+        (distinct (reduce (fn [a x]
+                            (conj a (apply map-difference x)))
+                          [] permutations))))))
--- test/mach_lear/utility_test.clj
+++ test/mach_lear/utility_test.clj
@@ -8,6 +8,18 @@
         result-sum {"alpha" 7 "beta" 11}]
     (is (= result-sum (map-intersection + map-a map-b)))))
 
+(deftest map-intersection-empty-test
+  (is (= {} (map-intersection {}))))
+
+(deftest map-intersection-empty-test-2
+  (is (= {} (map-intersection + {}))))
+
+(deftest map-intersection-empty-test-3
+  (is (= {} (map-intersection + {} {:alpha 2}))))
+
+(deftest map-intersection-empty-test-4
+  (is (= {} (map-intersection + {} {}))))
+
 (deftest get-common-keys-test
   (let [map-a {:omega 3 :tyri 4 :polo 4}
         map-b {:omega 5 :tyri 8}
@@ -30,6 +42,16 @@
         result {:alpha 3 :beta 49 :ceta 4 :gamma 78 :agatha 45}]
   (is (= result (map-union + map-a map-c map-b)))))
 
+(deftest map-union-empty-test
+  (is (= {} (map-union {}))))
+
+(deftest map-union-empty-test-2
+  (is (= {} (map-union + {} {}))))
+
+(deftest map-intersection-empty-test-3
+  (let [map-a {:alpha 2}]
+    (is (= map-a (map-union + {} map-a)))))
+
 (deftest map-difference-test-a
   (let [map-a  {:gamma 12 :golang 23 :jimmy 7}
         map-b  {:gamma 23}
@@ -44,6 +66,18 @@
         result {}]
     (is (= result (map-difference map-a map-b map-c)))))
 
+(deftest map-difference-empty-test
+  (is (= {} (map-difference {}))))
+
+(deftest map-difference-empty-test-2
+  (is (= {} (map-difference {} {:alpha "omega"}))))
+
+(deftest map-difference-empty-test-3
+  (is (= {} (map-difference {} {}))))
+
+(deftest map-difference-empty-test-4
+  (let [map-a {:gamma "gamma"}]
+    (is (= map-a (map-difference map-a {})))))
 
 (deftest map-xor-test
   (let [map-a  {:gamma 12 :golang 23 :jimmy 7}
@@ -51,3 +85,17 @@
         map-c  {:gamma 2 :jimmy 8 :daffy 7}
         result [{:golang 23} {} {:daffy 7}]]
         (is (= result (map-xor map-a map-b map-c)))))
+
+(deftest map-xor-empty-test
+  (is (= '({}) (map-xor {}))))
+
+(deftest map-xor-empty-test-2
+  (is (= '({} {}) (map-xor {} {}))))
+
+(deftest map-xor-empty-test-3
+  (let [map-a {:alpha "alpha" :beta "beta"}]
+     (is (= (seq [map-a {}]) (map-xor map-a {})))))
+
+(deftest map-xor-empty-test-4
+  (let [map-a {:alpha "alpha" :beta "beta"}]
+    (is (= (seq [{} {}]) (map-xor map-a map-a)))))
